5cdf4517ad7102ab0f2b907ab73694d68da4f409
nick fiorini <nf071590@gmail.com>
2015-09-24 20:30:55 -0400

    updates

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+var a;
+var x;
+var b = -99;
+if(some === 2){
+	 x = true
+}
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use('/', 		express.static(__dirname + '/www'));
 app.use('/login', 	express.static(__dirname + '/www/pages/login'))
 
 /** hold all static assets */
-app.use('/assets', 	express.static(__dirname + '/www/assets'));
+app.use('/assets', 	express.static(__dirname + '/assets'));
 
 
 var server 	= app.listen(3000);
