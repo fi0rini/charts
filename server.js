05d6e37c356cec751b5d31a60751811b13e38870
nick fiorini <nf071590@gmail.com>
2015-09-28 22:49:00 -0400

    updates and adding things

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express	= require('express');
-var app 	= express();
+var app 		= express();
 
 app.use('/', 		express.static(__dirname + '/web'));
 app.use('/login', 	express.static(__dirname + '/web/pages/login'))
 
 /** hold all static assets */
 app.use('/assets', 	express.static(__dirname + '/build'));
-
 
 var server 	= app.listen(3000);
