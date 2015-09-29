42d3cc3b396864ccbb655f2002d625dd9a15bea1
nick fiorini <nf071590@gmail.com>
2015-09-29 15:23:46 -0400

    foundational

--- README.md
+++ README.md
@@ -2,15 +2,11 @@
 
 ## Prerequisites
 1. [node](www.nodejs.org) & [npm](www.nodejs.org)
-2. [mongodb](www.mongodo.org)
 
 ## Getting Started
-Install local dependencies
->`npm i` 
-
 Install babel globally
->`npm i -g babel`
+`npm i -g babel`
 
 Start the local development server
->`npm run dev-server`
+`npm run dev-server`
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+var port 	= process.env.npm_package_config_port;
 var express	= require('express');
-var app 		= express();
+var app 	= express();
 
 app.use('/', 		express.static(__dirname + '/web'));
 app.use('/login', 	express.static(__dirname + '/web/pages/login'))
@@ -7,4 +8,6 @@ app.use('/login', 	express.static(__dirname + '/web/pages/login'))
 /** hold all static assets */
 app.use('/assets', 	express.static(__dirname + '/build'));
 
-var server 	= app.listen(3000);
+var server 	= app.listen(port);
+
+console.log('sever listening on ' + port);
